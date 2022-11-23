de8ce967803fd6e9d9ee4ee805de30009b067e4e
Your Name <surajpadhi208@gmail.com>
2022-11-23 11:29:20 +0530

    first commit

---
+++ project2/Untitled-1.cs
@@ -0,0 +1,17 @@
+function Main()
+
+{
+
+String str1 = "Mind", str2 = "Nerves";
+
+str1 = str1+ str2;
+
+str2= str1.Substring(0, (str1.Length - str2.Length));
+
+str1= str1.Substring(str2.Length);
+
+print("str1 + " " + str2);
+
+ction?(This is an algorit
+
+}
